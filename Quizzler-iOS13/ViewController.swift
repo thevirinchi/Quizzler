9d8eba632ea6f88c340cdf60c085090eb43af019
z3t4z00k <zetazook@gmail.com>
2020-10-17 23:25:55 +0530

    Added function to update questions and count score

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -2,11 +2,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+
+    let quiz = [
+        ["4 + 2 = 6", "True"],
+        ["3 + 5 = 8", "True"],
+        ["5 - 8 = 3", "False"]
+    ]
+    var quesNumber = -1;
+    var score = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI();
     }
 
-
+    @IBAction func answerButton(_ sender: UIButton) {
+        if (sender.currentTitle == quiz[quesNumber][1]){
+            score+=1
+            updateUI();
+        }
+        else{
+            updateUI();
+        }
+    }
+    
+    func updateUI(){
+        if(quesNumber+1<quiz.count){
+            quesNumber+=1;
+            questionLabel.text = quiz[quesNumber][0];
+        }
+        else{
+            questionLabel.text = "Your score was " + String(score) + "/" +  String(quiz.count);
+        }
+    }
 }
 
